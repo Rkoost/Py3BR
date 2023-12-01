62bdda45e416e2d2f2a56dc3fd5e6910af42a478
rian <rian.koots@stonybrook.edu>
2023-12-01 13:52:36 -0500

    Add autocm

--- Py3BR/constants.py
+++ Py3BR/constants.py
@@ -4,3 +4,4 @@ u2me = 1.8228884862e3      # atomic mass (amu or u) to m_e : atomic unit mass (a
 J2Har = 2.2937122783963e17
 Har2invcm = 2.194746313632e5
 ttos = 2.4188843265857e-17
+autocm = 5.2918e-9
